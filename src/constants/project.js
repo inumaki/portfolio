806a4d8b65ac5921d0a5ed681d208ef8b2713783
aryan <etoyora@gmail.com>
2023-12-07 21:37:36 +0530

    change path again

--- src/constants/project.js
+++ src/constants/project.js
@@ -3,7 +3,7 @@ const project = [
     title: "Epic Eats",
     description: "Food Ordering Website",
     skills: ["Reactjs", "SpringBoot", "Redis"],
-    src: "../assets/projects/epiceats.png",
+    src: "/src/assets/projects/epiceats.png",
     link: "https://github.com/inumaki/EpicEats",
   },
   {
