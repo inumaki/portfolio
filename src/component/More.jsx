5bdfdaf2969207d921e87484adcb252a023b4312
aryan <etoyora@gmail.com>
2023-12-14 22:26:46 +0530

    add  margin

--- src/component/More.jsx
+++ src/component/More.jsx
@@ -5,14 +5,11 @@ import { Link, useLocation } from "react-router-dom";
 import { path, text} from "../constants/constant";
 
 function More() {
-  const [direction, setDirection] = useState(1);
-  const [position, setPosition] = useState(0);
 
   var currentPath = useLocation().pathname;
   const nextPath = path.get(currentPath);
   const seeMoreText = text.get(currentPath);
-  
-
+  console.log(nextPath, seeMoreText);
   return (
     <>
       <div className="more-about">
@@ -20,7 +17,7 @@ function More() {
           <div className="about-me-section">
             <div>{seeMoreText}</div>
             <div className="arrow-container">
-              <div className="moving-arrow" style={{ left: `${position}px` }}>
+              <div className="moving-arrow">
                 <img
                   src={rightArrow}
                   style={{ height: "32px", width: "24px" }}
